3ba2839097b63d486c9bb27e5db9d74b24ec6c4c
VyRajaRao <vyraja777@gmail.com>
2026-05-05 10:50:03 +0530

    Change to deployment

--- src/components/footer.py
+++ src/components/footer.py
@@ -6,7 +6,7 @@ def footer_home():
     
     st.markdown(f"""
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
-        <p style="font-weight:bold; color:white;"> Created with ❤️ by </p>  
+        <p style="font-weight:bold; color:white;"> Created with <img src="https://streamlit.io/images/brand/streamlit-logo-primary-colormark-darktext.png" width="110"> by Raja </p>  
         <img src='{logo_url}' style='max-height:25px' />
         </div>
                 
@@ -18,7 +18,7 @@ def footer_dashboard():
     
     st.markdown(f"""
         <div style="margin-top:2rem; display:flex; gap:6px; justify-content:center; items-align:center">
-        <p style="font-weight:bold; color:black;"> Created with ❤️ by </p>  
+        <p style="font-weight:bold; color:black;"> Created with <img src="https://streamlit.io/images/brand/streamlit-logo-primary-colormark-darktext.png" width="110"> by Raja </p>  
         <img src='{logo_url}' style='max-height:25px' />
         </div>
                 
